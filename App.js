bf1981991625d86717a5d885a7b61b6747a1f23e
austriker27 <davidalindahl@gmail.com>
2020-03-03 14:48:17 -0800

    init commit thats late

---
+++ App.js
@@ -0,0 +1,65 @@
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+
+export default function App() {
+    const [enteredGoal, setEnteredGoal] = useState('');
+    const [courseGoals, setCourseGoals] = useState([]);
+
+    const goalInputHandler = (enteredText) => {
+        setEnteredGoal(enteredText);
+    };
+
+    const addGoalHandler = () => {
+        setCourseGoals([...courseGoals, enteredGoal]);
+    };
+
+    return (
+        <View style={styles.root}>
+            <View>
+                <Text style={styles.title}>Todo List Thingy</Text>
+            </View>
+            <View style={styles.container}>
+                <TextInput 
+                    placeholder="" 
+                    style={styles.inputContainer} 
+                    onChangeText={goalInputHandler}
+                    value={enteredGoal}
+                />
+                <Button style={styles.button} title="ADD" onPress={addGoalHandler} />
+            </View>
+            <View>
+                
+
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    root: {
+        padding: 50,
+        backgroundColor: '#1C0658',
+        height: '100%',
+    },
+    container: {
+        flexDirection: 'row',
+        flex: 1,
+        color: '#FF1690',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    inputContainer: {
+        borderColor: '#FF1690', 
+        color: '#FF1690',
+        borderWidth: 1, 
+        padding: 10, 
+        width: '80%',
+    },  
+    button: {
+        backgroundColor: '#36CDC4',
+        color: '#FF1690',
+    },
+    title: {
+        color: '#FF1690',
+    }
+});
