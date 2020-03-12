a77516e578aeffb94e560e465ab4ee6ad05e59cd
austriker27 <davidalindahl@gmail.com>
2020-03-12 07:10:52 -0700

    added header component and screens

--- guess-number/App.js
+++ guess-number/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen';
+
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+        <Header title="Heyooo, guess a number"/>
+        <StartGameScreen/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
