c04089e406cae15cb3673dd0dbbde9898eb553d2
austriker27 <davidalindahl@gmail.com>
2020-03-18 10:14:39 -0700

    adding guessing logic

--- guess-number/screens/GameScreen.js
+++ guess-number/screens/GameScreen.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { View, Text, StyleSheet, Button, Alert } from 'react-native';
 
 import NumberContainer from '../components/NumberContainer';
 import Card from '../components/Card';
@@ -18,15 +18,43 @@ const generateRandomBetween = (min, max, exclude) => {
 }
 
 const GameScreen = props => {
-    const [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1, 100, props.userChoice));
+    const [currentGuess, setCurrentGuess] = useState(
+        generateRandomBetween(1, 100, props.userChoice)
+    );
+
+    // useRef maintains even after component rebuild
+    // if state changes then the component rerenders, but with useRef, when it changes, the component doesnt rerender. we dont want a rerender when these change
+    const currentLow = useRef(1);
+    const currentHigh = useRef(100);
+
+    const nextGuessHandler = direction => {
+        if (
+            (direction === 'lower' && currentGuess < props.userChoice) || 
+            (direction === 'great' && currentGuess > props.userChoice))
+            {
+                Alert.alert('Don\'t lie!', 'You now that this is wrong', [
+                    { text: 'Sorry!', style: 'cancel' }
+                ])
+                return;
+        }
+        // if give correct hint, then generate new number
+        if (direction === 'lower'){
+            currentHigh.current = currentGuess;
+        } else {
+            currentLow.current = currentGuess;
+        }
+        const nextNumber = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess);
+        // component rerendered and next guess:
+        setCurrentGuess(nextNumber);
+    }
 
     return (
         <View style={styles.screen}>
             <Text>Opponent's Guess</Text>
             <NumberContainer>{currentGuess}</NumberContainer>
             <Card style={styles.buttonContainer}>
-                <Button title="LOWER" onPress={() => {}} />
-                <Button title="GREATER" onPress={() => {}}/>
+                <Button title="LOWER" onPress={nextGuessHandler.bind(this, 'lower')} />
+                <Button title="GREATER" onPress={nextGuessHandler.bind(this, 'greater')}/>
             </Card>
         </View>
     )
