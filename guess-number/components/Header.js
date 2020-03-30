09fbc8a78882015bd756927f21063f5129219650
austriker27 <davidalindahl@gmail.com>
2020-03-30 09:44:14 -0700

    header revactor

--- guess-number/components/Header.js
+++ guess-number/components/Header.js
@@ -6,22 +6,30 @@ import Colors from '../constants/colors';
 
 const Header = props => {
     return (
-        <View style={styles.header}>
+        <View style={{...styles.headerBase, ...Platform.select({ios: styles.headerIos, android: styles.headerAndroid})}}>
             <TitleText style={styles.headerTitle}> {props.title} </TitleText>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    header: {
+    headerBase: {
         width: '100%',
         height: 90,
         paddingTop: 36,
-        backgroundColor: Platform.OS === 'android' ? Colors.primary : 'white',
         alignItems: 'center',
         justifyContent: 'center',
-        borderBottomColor: Platform.OS === 'ios' ? '#ccc' : 'transparent',
-        borderBottomWidth: Platform.OS === 'ios' ? 1 : 0,
+        
+    },
+    headerIos: {
+        backgroundColor: 'white',
+        borderBottomColor:  '#ccc',
+        borderBottomWidth:1,
+    },
+    headerAndroid: {
+        backgroundColor: Colors.primary,
+        borderBottomColor: 'transparent',
+        borderBottomWidth: 0,
     },
     headerTitle: {
         color:  Platform.OS === 'ios' ? Colors.primary : 'white'
